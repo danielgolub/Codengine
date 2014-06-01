d1b8375c8aaa3868b1ec241719beeef536013317
Daniel Golub <danielg6666@me.com>
2014-06-01 17:48:32 +0300

    added string validation options

--- app/base/Security/Security.base.php
+++ app/base/Security/Security.base.php
@@ -55,6 +55,46 @@ html;
 		else
 			die("Function destroy_session requires an array");
 	}
+
+	public function validate($str, $mode)
+	{
+		switch ($mode) {
+			case 'email':
+				if(filter_var($str, FILTER_VALIDATE_EMAIL))
+					return true;
+				else
+					return false;
+				break;
+
+			case 'int':
+				if(filter_var($str, FILTER_VALIDATE_INT))
+					return true;
+				else
+					return false;
+				break;
+
+			case 'phone':
+				if(preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $str))
+					return true;
+				else
+					return false;
+				break;
+
+			case 'ip':
+				if(filter_var($str, FILTER_VALIDATE_IP))
+					return true;
+				else
+					return false;
+				break;
+
+			case 'url':
+				if(filter_var($str, FILTER_VALIDATE_URL))
+					return true;
+				else
+					return false;
+				break;
+		}
+	}
 }
 
 ?>
