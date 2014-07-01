0aece65c5b5240fdca837c8dee9765e7236e0eb0
Daniel Golub <danielg6666@me.com>
2014-07-01 20:14:30 +0300

    link to documentation fixed

--- app/views/welcome/index.php
+++ app/views/welcome/index.php
@@ -3,5 +3,5 @@
 			<div class="jumbotron">
 				<h1><?php print_r($strings); ?>, world!</h1>
 				<p>Your Codengine app works and ready to use.</p>
-				<p><a href="https://github.com/danielgolub/Codengine/wiki" class="btn btn-primary btn-lg" role="button" target="_blank">Learn more &raquo;</a></p>
+				<p><a href="https://github.com/danielgolub/Codengine/wiki/Documentation" class="btn btn-primary btn-lg" role="button" target="_blank">Learn more &raquo;</a></p>
 			</div>
